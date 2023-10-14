babf2450eb802bfacb96fdb0163b8677db90c46f
SajeedAninda <sajaninda@gmail.com>
2023-10-14 16:07:26 +0600

    Added Add Coffee section

--- coffee-client/src/Components/Homepage.jsx
+++ coffee-client/src/Components/Homepage.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import Banner from './Banner/Banner';
 import SubBanner from './SubBanner/SubBanner';
 import Insta from './Instagram/Insta';
+import Products from './Products/Products';
 
 const Homepage = () => {
     return (
         <div>
             <Banner></Banner>
             <SubBanner></SubBanner>
+            <Products></Products>
             <Insta></Insta>
         </div>
     );
