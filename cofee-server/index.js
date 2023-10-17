803a2726ee7997b3dbcb7ad5654be6dd3a287d77
SajeedAninda <sajaninda@gmail.com>
2023-10-17 15:28:33 +0600

    Minor

--- cofee-server/index.js
+++ cofee-server/index.js
@@ -4,13 +4,7 @@ let app = express();
 let cors = require("cors");
 let port = process.env.PORT || 5000;
 // MIDDLEWARE
-const corsConfig = {
-    origin: '',
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE']
-}
-app.use(cors(corsConfig))
-app.options("", cors(corsConfig))
+app.use(cors());
 
 
 app.use(express.json());
