ebd70dca9094a4656c215e99a25b30311e4f32ea
SajeedAninda <sajaninda@gmail.com>
2023-10-17 14:51:42 +0600

    Minor Changes

--- cofee-server/index.js
+++ cofee-server/index.js
@@ -19,7 +19,7 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
         const coffeeCollection = client.db("coffeesDB").collection("coffees");
 
         app.post("/coffees", async (req, res) => {
@@ -65,7 +65,7 @@ async function run() {
             res.send(result);
         });
         // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
+        // await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
